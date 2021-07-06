f608eb57c048bffbe6fa72ae76314bed8fc545c4
joannakot <joanna.kot.ok@gmail.com>
2021-07-06 19:09:06 +0200

    homepage fixed and styling works

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,7 +1,7 @@
 // this will be placed in public folder where script.js is located 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg';
-
+console.log(mapboxgl);
 const map = new mapboxgl.Map({
 	container: 'map', 
 	style: 'mapbox://styles/joannakt/ckqqgjbik3ant17mn9epqpvcm', 
