68fcba319c95e37170a47e964b422bf5297564bb
joannakot <joanna.kot.ok@gmail.com>
2021-07-07 18:00:11 +0200

    styling finished

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,5 +1,6 @@
 // this will be placed in public folder where script.js is located 
 
+// const { default: axios } = require('axios');
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg';
 console.log(mapboxgl);
 const map = new mapboxgl.Map({
@@ -8,6 +9,15 @@ const map = new mapboxgl.Map({
 	center: [13.426, 52.531], 
 	zoom: 9, 
 });
+
+// let testAdd ='Koppenstraße 3, 10243 Berlin'
+// let testOutput = {};
+// axios.get(testAdd)
+//     .then(response => {
+//         console.log("Checking address:", testAdd)
+//         testOutput.push(axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${testAdd}.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg`))
+//         console.log("Checking mapbox API:", testOutput)
+//     })
 
 const nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
@@ -18,3 +28,5 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+
