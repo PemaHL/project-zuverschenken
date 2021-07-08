6ad91b122ef850f642aba0a7012466f0f4c49c3d
Hanlin Li <lihanlinapp@gmail.com>
2021-07-08 14:44:21 +0200

    fixed weird problems in unexplainable way, hope it works now

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Box = require('../models/Box');
 const User = require('../models/User.model');
-// const axios = require('axios');
+const axios = require('axios');
 
 /* GET home page */
 //the homepage should show all boxes on the map - this need to be added - done
@@ -18,21 +18,23 @@ router.get("/", (req, res, next) => {
       })
 });
 
-//router.get("/", (req, res, next) => {
-  //axios.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q='+"Koppenstraße 3, 10243 Berlin")
-  //axios.get('https://nominatim.openstreetmap.org/search?<params>  ')
-  //.then(response => {
-  //  console.log("need to test api");
-  //  console.log(response);
-  //})
-  //.catch(err => {
-  //  console.log(err)
-  //  })
-//});
-
-
-//axios.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q='+"Koppenstraße 3, 10243 Berlin", function(data){
-//  console.log(data);
-//});
+//make a route for the data for axios
+//let boxesListData = [];
+router.get("/data", (req, res, next) => {
+  Box.find()
+      .then(boxesList => {
+        //boxesListData.push(boxesList);
+        //console.log("2 checking if box data is pushed to new object:", boxesListData)
+        address = boxesList.map(e => encodeURI(e.address));
+       // address = boxesList.map(e => encodeURIComponent(e.address));
+       // address = boxesList.map(e => e.address);
+        console.log("2 testing address:", address)
+        res.json({input:address});
+        //console.log("checking to retrieve address", boxesList.address)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+});
 
 module.exports = router;
