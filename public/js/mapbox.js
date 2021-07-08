8ce391c8cd78307afce2d399d328de2a1a2ba470
Hanlin Li <lihanlinapp@gmail.com>
2021-07-08 11:54:06 +0200

    address to geo fixed

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,8 +1,6 @@
 // this will be placed in public folder where script.js is located 
-//<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-const { default: axios } = require("axios");
+//const { default: axios } = require("axios");
 
-// const { default: axios } = require('axios');
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg';
 console.log(mapboxgl);
 const map = new mapboxgl.Map({
@@ -13,30 +11,62 @@ const map = new mapboxgl.Map({
 });
 
 let markers = [];
-axios.get('http://localhost:3000/index')
-        .then(response => {
-            console.log(response);
-            response.boxesList.address.forEach(postalCode => {
-                //console.log(postalCode);
-                axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg')
-                .then(response=> {
-                    //console.log(response.data.features[0].geometry.coordinates);
-                    markers.push(response.data.features[0].geometry.coordinates);
-					//not sure if .data need to be renamed, since I cannot see the output yet
-                    console.log(markers);
-                    markers.forEach(coord => {
-                        new mapboxgl.Marker({
-                            color: 'red'
-                        })
-                        .setLngLat(coord)
-                        .addTo(map)
-                    })  
-                })
-            })          
-        })
-        .catch(err => {
-            console.log(err);
-        })
+  axios.get('http://localhost:3000/data')
+          .then(response => {
+               console.log("3 checking if axios is reading the object :", response.data.input);
+               response.data.input.forEach(address => {
+                   console.log("4 checking if axios is reading the address:", address);
+				   axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}%20Germany.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg`)
+                   //axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}%20Berlin%20Germany.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg')
+                   //123%20Main%20St%20Boston%20MA
+				  //axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Kottbusser%20Damm%2072%2010967%20Berlin.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg')
+				   .then(response=> {
+                      // console.log("5 checking data returned by mapbox api:",response.data.features[1].geometry.coordinates);
+					   console.log("5 checking data returned by mapbox api:",response.data);
+                       markers.push(response.data.features[0].geometry.coordinates);
+                        console.log("6 checking geo coordinates array:", markers);
+						markers.forEach(coord => {
+							new mapboxgl.Marker({
+							color: 'blue'
+							})
+							.setLngLat(coord)
+							.addTo(map)
+							})  
+                      })
+                    })
+                  })
+            .catch(err => {
+                console.log(err);
+            })
+
+
+
+// let markers = [];
+// //import { get } from 'axios';
+// get('http://localhost:3000/index')
+//         .then(response => {
+//             console.log(response);
+//             response.boxesList.address.forEach(postalCode => {
+//                 //console.log(postalCode);
+//                 get('https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoiam9hbm5ha3QiLCJhIjoiY2txcWZveDRzMDBpajJ2bm9vbnVwOTVwaCJ9.viWCugOjLMuoOcdVH-2ewg')
+//                 .then(response=> {
+//                     //console.log(response.data.features[0].geometry.coordinates);
+//                     markers.push(response.data.features[0].geometry.coordinates);
+// 					//not sure if .data need to be renamed, since I cannot see the output yet
+//                      console.log(markers);
+//                      markers.forEach(coord => {
+//                          new mapboxgl.Marker({
+//                              color: 'red'
+//                          })
+//                          .setLngLat(coord)
+//                          .addTo(map)
+//                      })  
+//                  })
+//              })          
+//          })
+//          .catch(err => {
+//              console.log(err);
+//          })
 
 
 // let testAdd ='Koppenstraße 3, 10243 Berlin'
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,56 @@ const axios = require('axios');
 //the homepage should show all boxes on the map - this need to be added - done
 //how would we know the location of the box?
 
+//original code before playing with axios & api
+// router.get("/", (req, res, next) => {
+//   console.log("testing homepage");
+//   Box.find()
+//       .then(boxesList => {
+//         res.render("index", {boxesList});
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+// });
 router.get("/", (req, res, next) => {
-  console.log("testing homepage");
+  console.log("1 testing homepage");
   Box.find()
       .then(boxesList => {
+       // boxesListData.push(boxesList);
+       // console.log("2 checking if box data is pushed to new object:", boxesListData)
         res.render("index", {boxesList});
+        //console.log("checking to retrieve address", boxesList.address)
       })
       .catch(err => {
         console.log(err)
       })
 });
+
+
+
+
+//make a route for the data for axios
+//let boxesListData = [];
+router.get("/data", (req, res, next) => {
+  Box.find()
+      .then(boxesList => {
+        //boxesListData.push(boxesList);
+        //console.log("2 checking if box data is pushed to new object:", boxesListData)
+        address = boxesList.map(e => encodeURI(e.address));
+       // address = boxesList.map(e => encodeURIComponent(e.address));
+       // address = boxesList.map(e => e.address);
+        console.log("2 testing address:", address)
+        res.json({input:address});
+        //console.log("checking to retrieve address", boxesList.address)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+});
+//console.log("checking if box data is pushed to new object:", boxesListData)
+
+  
+
 
 //router.get("/", (req, res, next) => {
   //axios.get(location.protocol + '//nominatim.openstreetmap.org/search?format=json&q='+"Koppenstraße 3, 10243 Berlin")
